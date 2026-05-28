a7388c42e757d7484c9b90c46bb59e0c7fb53f6c
mahta mira <mahta.m.1183@gmail.com>
2026-05-28 14:28:08 +0330

    add new model Task

--- clockify/contracts/admin.py
+++ clockify/contracts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Contract
+from .models import FreelancerContract, EmployerContract
 # Register your models here.
-admin.site.register(Contract)
+admin.site.register(EmployerContract)
+admin.site.register(FreelancerContract)
--- clockify/teams/models.py
+++ clockify/teams/models.py
@@ -5,22 +5,28 @@ from django.utils.text import slugify
 from django.conf import settings
 from django.dispatch import receiver
 from django.db.models.signals import post_delete
+from users.models import User
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+
 
 class Team(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(max_length=100,unique=True)
-    description = models.TextField(blank=True,max_length=300)
-    logo = models.ImageField(upload_to='team_logos/', blank=True, null=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(blank=True, max_length=300)
+    logo = models.ImageField(upload_to="team_logos/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    supervisor = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.PROTECT,related_name="supervised_teams",null=True,blank=False)
+    supervisor = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name="supervised_teams",
+        null=True,
+        blank=False,
+    )
     members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="teams")
 
-
-    # supervisor has to exist in the list of members for each team
-    
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
@@ -31,11 +37,61 @@ class Team(models.Model):
         return self.name
 
 
+class Task(models.Model):
+
+    class Priority(models.TextChoices):
+        HIGH = "HIGH", "High"
+        MEDIUM = "MEDIUM", "Medium"
+        LOW = "LOW", "Low"
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="tasks")
+
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="created_tasks"
+    )
+    assigned_to = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="assigned_tasks"
+    )
+
+    deadline = models.DateTimeField()
+    priority = models.CharField(
+        max_length=10, choices=Priority.choices, default=Priority.MEDIUM
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def clean(self):
+        super().clean()
+
+        if self.team and self.created_by and self.team.supervisor != self.created_by:
+            raise ValidationError(
+                {
+                    "created_by": f"{self.created_by.email} is not designated as the supervisor for the team '{self.team.name}'."
+                }
+            )
+
+        if (
+            self.team
+            and self.assigned_to
+            and self.assigned_to not in self.team.members.all()
+        ):
+            raise ValidationError(
+                {
+                    "assigned_to": f"{self.assigned_to.email} is not a member of the team '{self.team.name}'."
+                }
+            )
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+
 @receiver(post_delete, sender=Team)
 def auto_delete_file_on_delete(sender, instance, **kwargs):
-        if instance.logo:
-            if os.path.isfile(instance.logo.path):
-                os.remove(instance.logo.path)
+    if instance.logo:
+        if os.path.isfile(instance.logo.path):
+            os.remove(instance.logo.path)
 
 
 @receiver(models.signals.pre_save, sender=Team)
@@ -50,7 +106,6 @@ def auto_delete_file_on_change(instance, **kwargs):
 
     new_logo = instance.logo
 
-    if old_logo and old_logo != new_logo :
+    if old_logo and old_logo != new_logo:
         if os.path.isfile(old_logo.path):
             os.remove(old_logo.path)
- 
