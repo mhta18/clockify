298ce021a3577193025e6c273578472e75a80ac7
mahta mira <mahta.m.1183@gmail.com>
2026-06-10 00:19:02 +0330

    chore: tell git to permanently ignore root folder

--- clockify/teams/permissions.py
+++ clockify/teams/permissions.py
@@ -9,7 +9,7 @@ class IsObjectWorkerOrSupervisor(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
 
-        is_worker = obj.assigned_to = request.user
+        is_worker = obj.assigned_to == request.user 
 
-        is_supervisor = obj.team and obj.team.supervisor == request.user
+        is_supervisor = obj.team and (obj.team.supervisor == request.user)
         return is_worker or is_supervisor
